d60df141081a76e9d54e73d83a84efbc4914f4bd
giantthinker <ramadanabdel24@gmail.com>
2019-12-11 08:58:42 +0100

    Edit the Contact Page

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -7,7 +7,7 @@
     <v-container fluid style="border-color:#e0e0e0;border-width: 1px;border-style: solid;border-bottom: 0">
       <v-layout wrap align-center justify-center row fill-height class="my-0 py-0">
         <v-flex xs12 md10 sm10 lg10 class="py-0 my-0">
-          <p class="google-font" style="font-size:130%" >Follow Us:
+          <p class="google-font" style="font-size:130%" >Suivez Nous:
             <span v-for="(item,i) in ChapterDetails.socialLinks" :key="i">
               <v-tooltip top>
                   <v-btn flat icon :href="item.link" target="_blank" slot="activator">
